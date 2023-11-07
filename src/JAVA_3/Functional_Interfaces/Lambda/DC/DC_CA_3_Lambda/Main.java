8bba610adf2b17ea8177a44940ebbc9ba3423301
MicFro <michalfrost71@gmail.com>
2023-11-07 20:13:29 +0100

    commit init

---
+++ src/JAVA_3/Functional_Interfaces/Lambda/DC/DC_CA_3_Lambda/Main.java
@@ -0,0 +1,4 @@
+package JAVA_3.Functional_Interfaces.Lambda.DC.DC_CA_3_Lambda;
+
+public class Main {
+}
---
+++ src/JAVA_3/Functional_Interfaces/Lambda/DC/DC_T_6_Lambda_Tempera/LambdaExercise.java
@@ -0,0 +1,4 @@
+package JAVA_3.Functional_Interfaces.Lambda.DC.DC_T_6_Lambda_Tempera;
+
+public class LambdaExercise {
+}
